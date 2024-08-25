aa0f2227847d0867f1a637948bf0911bf220a972
lina <anabandonedone@outlook.com>
2024-08-25 17:17:54 +0800

    modified:   snake.py

--- snake.py
+++ snake.py
@@ -1,1 +1,46 @@
 import random
+import sys
+import tty
+import termios
+
+def readchar():
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    try:
+        tty.setraw(sys.stdin.fileno())
+        ch = sys.stdin.read(1)
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    return ch
+
+def readkey(getchar_fn=None):
+    getchar = getchar_fn or readchar
+    c1 = getchar()
+    if ord(c1) != 0x1b:
+        return c1
+    c2 = getchar()
+    if ord(c2) != 0x5b:
+        return c1
+    c3 = getchar()
+    return chr(0x10 + ord(c3) - 65)
+class FaceEnum:
+    UP=(-1,0)
+    DOWN=(1,0)
+    LEFT=(0,-1)
+    RIGHT=(0,1)
+class Map:
+    def __init__(self,height,width):
+        self.scale=(height,width)
+        self.map=[[' ' for _ in range(width)] for _ in range(height)]
+        self.snake=Snake(self)
+class Snake:   
+    def __init__(self,map):
+        randomPos=lambda:(random.randint(0,map.scale.height-1),random.randint(0,map.scale.width-1))     
+        self.head=randomPos()
+        self.body=[]
+    def move(self,direction,isEated=False):
+        if isEated:
+            self.body.append((self.head[0],self.head[1]))
+        self.head[0]+=direction[0]
+        self.head[1]+=direction[1]
+        
