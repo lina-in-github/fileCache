72396ca484c3844ba37cb66ead4c977e8ca19812
lina <anabandonedone@outlook.com>
2024-08-26 11:36:10 +0800

    modified:   snake.py

--- snake.py
+++ snake.py
@@ -1,75 +1,107 @@
 import random
 import sys
 import time
+import threading
+import os
 import keyboard
+pressedkey='w'
 def readkey():
-    ...
+    class p(threading.Thread):
+        def __init__(self):
+            threading.Thread.__init__(self)
+            self.daemon = True
+            self.setDaemon(True)
+            self.start()
+        def run(self):
+            global pressedkey
+            while True:
+                if keyboard.is_pressed('w'):
+                    pressedkey='w'
+                if keyboard.is_pressed('a'):
+                    pressedkey='a'
+                if keyboard.is_pressed('s'):
+                    pressedkey='s'
+                if keyboard.is_pressed('d'):
+                    pressedkey='d'
+                time.sleep(0.01)
+    p()
+    while True:
+        yield pressedkey
 class FaceEnum:
-    UP=(-1,0)
-    DOWN=(1,0)
-    LEFT=(0,-1)
-    RIGHT=(0,1)
-    def __init__(self, char):
+    LEFT=(-1,0)
+    RIGHT=(1,0)
+    UP=(0,-1)
+    DOWN=(0,1)
+    @staticmethod
+    def get(char):
         if char == 'w':
-            self.vector=FaceEnum.UP
+            return FaceEnum.UP
         if char =='a':
-            self.vector=FaceEnum.LEFT
+            return FaceEnum.LEFT
         if char =='s':
-            self.vector=FaceEnum.DOWN
+            return FaceEnum.DOWN
         if char =='d':
-            self.vector=FaceEnum.RIGHT
+            return FaceEnum.RIGHT
 class Map:
     def __init__(self,height,width):
         self.scale=(height,width)
         self.map=[[' ' for _ in range(width)] for _ in range(height)]
         self.snake=Snake(self)
         self.food=(random.randint(0,height-1),random.randint(0,width-1))
-    def render(self,Head_Char='@',Body_Char='#'):
-        sys.stdout.clear()
-        for y,row in enumerate(self.map):
-            for x,cell in enumerate(row):
-                if (x,y)==self.snake.head:
-                    sys.stdout.write(Head_Char)
-                elif (x,y) in self.snake.body:
-                    sys.stdout.write(Body_Char)
+    def render(self, Head_Char='@', Body_Char='#',Food_Char='*'):
+        #sdwadwsys.stdout.write('\033[H\033[2J')  # Clear the screen and move the cursor to the top-left corner
+        for y, row in enumerate(self.map):
+            for x, cell in enumerate(row):
+                if (x, y) == self.snake.head:
+                    self.map[y][x] =Head_Char
+                elif (x, y) in self.snake.body:
+                    self.map[y][x] = Body_Char
+                elif (x, y) == self.food:
+                    self.map[y][x] = Food_Char
                 else:
-                    sys.stdout.write(cell)
-            sys.stdout.write('\n')
+                    self.map[y][x] =' '
+                #sys.stdout.write(cell)
     def mainloop(self, faceEnumGenerator):
-        while True:
-            faceEnum=faceEnumGenerator.next()
+        for faceEnum in faceEnumGenerator:
             try:
                 targ = False
                 time.sleep(0.5)
+                os.system('cls' if os.name == 'nt' else 'clear')  # FIXME:Fail to clear screen
                 if self.snake.head == self.food:
                     targ = True
-                    self.food = (random.randint(0, self.scale.height-1), random.randint(0, self.scale.width-1))
+                    self.food = (random.randint(0, self.scale[0]-1), random.randint(0, self.scale[1]-1))
                 if self.snake.head in self.snake.body:
                     break
                 self.snake.move(faceEnum, targ)
                 self.render()  
-            except:
+                for line in self.map:
+                    for cell in line:
+                        sys.stdout.write(cell)
+                    sys.stdout.write('\n')
+                
+            except IndexError as e:
+                raise
                 break  
-        sys.stdout.clear()
-        print(f'Game Over!,your length is:{len(self.snake.body)}')    
+            #print('\033[2J')
+        print(f'Game Over!,your length is:{len(self.snake.body)}')
 
 class Snake:   
     def __init__(self,map):
-        randomPos=lambda:(random.randint(0,map.scale.height-1),random.randint(0,map.scale.width-1))     
+        randomPos=lambda:(random.randint(0,map.scale[0]),random.randint(0,map.scale[1]-1))     
         self.head=randomPos()
         self.body=[]
     def move(self, direction, isEated=False):
-        self.head[0] += direction[0]
-        self.head[1] += direction[1]
-        tmp=self.body[-1][:]
+        self.head=(self.head[0]+direction[0],self.head[1]+direction[1])
+        tmp=self.body[len(self.body)-1][:] if len(self.body)!=0 else self.head
         self.body = [(i[0] + direction[0], i[1] + direction[1]) for i in self.body]  # Fixed: Use 'self.body' instead of 'body'
         if isEated:
             self.body.append(tmp)
 def faceEnumGenerator():
     directions=[FaceEnum.UP,FaceEnum.DOWN,FaceEnum.LEFT,FaceEnum.RIGHT]
+    rk=readkey()
     while True:
-        yield FaceEnum(readkey()).vector
-
+        yield FaceEnum.get(rk.__next__())
+print('\033[?25l')
 if __name__=='__main__':
     map=Map(20,30)
     map.mainloop(faceEnumGenerator())
